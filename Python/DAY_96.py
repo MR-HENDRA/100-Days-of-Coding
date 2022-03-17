92c821607b1e7150f4eb88ffd9ffacbce63b62ef
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-03-17 23:31:15 +0800

    List

---
+++ Python/DAY_96.py
@@ -0,0 +1,27 @@
+# TIPE DATA LIST
+
+# list kosong
+list_kosong = [] #1
+print('list_kosong:', list_kosong) #2
+
+# list yang berisi kumpulan string
+list_buah = ['Rambutan', 'Nangka', 'Melon', 'Durian'] #1
+print('list_buah:', list_buah) #2
+
+# list yang berisi kumpulan integer
+list_nilai = [75, 43, 69, 80]
+print('list_nilai:', list_nilai)
+
+# list campuran berbagai tipe data
+list_jawaban = [100, 45.7, 'Hendra', True]
+print('list_jawaban:', list_jawaban)
+
+# menampilkan isi tertentu dari list dengan indeks
+print(list_buah[0])
+print(list_buah[2])
+
+# menampilkan isi data dari belakang
+print(list_buah[-2])
+print(list_buah[-3])
+
+
