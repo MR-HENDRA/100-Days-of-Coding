cc4c01b13130742906e1b3c436600156a92eb117
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-03-21 23:18:35 +0800

    Update Game

--- Python/DAY_100.py
+++ Python/DAY_100.py
@@ -1,17 +1,20 @@
-player1 = {"nama":"Fyan","power":100}
-player2 = {"nama":"Hendra","power":250}
+print("MARI BERTARUNG DAN JADI PEMENANG !".center(30,"="))
+
+player1 = {"nama":"Hendra","power":100}
+player2 = {"nama":"Fyan","power":250}
 
 def train(player):
     player["power"] += 100 # def latihan, untuk menambah power
     
 def attack(attacker,defender):
     if(attacker["power"] > defender["power"]):
-        print("Serangan berhasil! Selamat untuk ", attacker["nama"])
+        print("Serangan berhasil! Kamu Hebat", attacker["nama"])
     else :
-        print("Serangan Gagal! Kamu Lemah ! ",attacker["nama"])
+        print("Serangan Gagal! Kamu Lemah",attacker["nama"])
 
 train(player1) # Latihan pertama player 1
 train(player1) # Latihan kedua player 1
 train(player2) # Latihan pertama player 2
+train(player1) # Latihan kedua player 2
 attack(player1,player2)
 
